7fffbf5b2bccc2af41ada514d999c0654507af21
Santiago Cruz <sforerocr@hotmail.com>
2023-05-30 19:44:10 -0500

    se creo panel.js el cual contiene un panel de control sobre las acciones en la lista de tareas, tambien contiene funciones asincronas. Se modificaron las funciones existentes para que retornen promesas

---
+++ README.md
@@ -0,0 +1,9 @@
+¿Qué sucedio al usar async y await?
+    Usando una funcion asincrona en panel.js se puede administrar el mostrar el resultado 
+    de las funciones que retornan promesas segun se necesite, pasando como parametros desde la funcion asincrona. LAs funciones qure retornan promesas corren al mismo tiempo
+¿Qué sucedio al usar el método then()?
+    Con el metodo then solo llamabamos una funcion de manera lineal a la vez, no lo 
+    encontré especialmente util para este ejercicio
+¿Qué diferencias encontraste entre async, await y el método then()?
+    el manejo del funcionamiento en las funciones llamadas, usando async y await las funciones
+    corren independientementey no de formal lineal como con then().
--- add.js
+++ add.js
@@ -4,24 +4,16 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-const add= () => {
-  rl.question('Ingrese el nombre: ', (nombre) => {
-    rl.question('Ingrese la descripción: ', (descripcion) => {
-      rl.question('Ingrese el estado: ', (estado) => {
+const add= (nombre, descripcion, estado) => {
+    return new Promise((resolve, reject) => {
         const task= {
             nombre: nombre,
             desc: descripcion,
             status: estado
         };
         taskList.push(task);
-        console.log(taskList);
-        if (nombre== "a"){
-            deleted();
-        }
-        rl.close();
-      });
+        resolve(taskList);
     });
-  });
 };
 add();
 module.exports = add;
